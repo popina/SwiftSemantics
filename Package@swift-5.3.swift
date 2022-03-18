7e73ddcb991f81dfbf233bffbc1b66176a7e3141
Masto <fonkymasto@gmail.com>
2022-03-18 10:07:51 +0100

    swift 5.6

--- Package@swift-5.3.swift
+++ Package@swift-5.3.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,19 +14,16 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-syntax.git",
-                 branch: "0.50600.1"),
+        .package(name: "SwiftSyntax",
+                 url: "https://github.com/apple/swift-syntax.git",
+                 .exact("0.50300.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftSemantics",
-            dependencies: [
-                .product(name: "SwiftSyntax", package: "swift-syntax"),
-                .product(name: "SwiftSyntaxParser", package: "swift-syntax"),
-                .product(name: "SwiftSyntaxBuilder", package: "swift-syntax"),
-            ]
+            dependencies: ["SwiftSyntax"]
         ),
         .testTarget(
             name: "SwiftSemanticsTests",
--- Tests/SwiftSemanticsTests/Extensions/SyntaxParser+Declarations.swift
+++ Tests/SwiftSemanticsTests/Extensions/SyntaxParser+Declarations.swift
@@ -1,4 +1,5 @@
 import SwiftSyntax
+import SwiftSyntaxParser
 import SwiftSemantics
 import struct SwiftSemantics.Protocol
 

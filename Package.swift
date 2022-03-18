c3528240f5a9950562bb727e211857df83dcb213
Masto <fonkymasto@gmail.com>
2022-03-18 10:11:20 +0100

    use tag instead of branch

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-syntax.git",
-                 branch: "0.50600.1"),
+                 from: "0.50600.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
